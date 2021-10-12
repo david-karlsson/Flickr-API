8cebca5ac35dfc09b7802b5b44b842b36ea1076d
David Karlsson <david.e.karlsson@telia.com>
2021-10-12 10:54:50 +0200

    image-url-building test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const searchTerm = document.querySelector('#search');//
 
 //     e.preventDefault();
 
-//     lista.innerHTML = '';
+//     search-list.innerHTML = '';
 //     getData(searchTerm.value);
 
 
@@ -29,12 +29,16 @@ async function getData(query) {
 
 
 
+
+
+
+
     const data = await response.json();
     // showPhotos(data.results);
 
 
 
-    console.log(data);
+ 
 
 
 
@@ -58,7 +62,8 @@ async function getData(query) {
         let SingePhotoObjectAdress = data.photos.photo[index]
         
         // let SinglePhotoUrl ="https://farm" + SingePhotoObjectAdress.farm-id + ".staticflickr.com/" + SingePhotoObjectAdress.server+"/" + SingePhotoObjectAdress.id + "_"+SingePhotoObjectAdress.secret + ".jpg";
-        
+        let SinglePhotoUrl = "https://live.staticflickr.com/" + SingePhotoObjectAdress.server + "/" + SingePhotoObjectAdress.id +"_" +SingePhotoObjectAdress.secret+".jpg";
+
         
         // "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=ca370d51a054836007519a00ff4ce59e&photo_id="+data.photos.photo[index].id+"&format=json&nojsoncallback=1";
 
@@ -68,15 +73,38 @@ async function getData(query) {
         lista.appendChild(item);
         item.innerHTML = `
         <h2> Image Title:"${data.photos.photo[index].title}"</h2>
-        <img src="" alt="${data.photos.photo[index].title}"/> `;
+        <img src= ${SinglePhotoUrl} alt="${data.photos.photo[index].title}"/> `;
 
 
         // const element = array[index];
 
     };
-
+    console.log(data);
 
 };
+
+
+
+
+async function getSearchData(query) {
+
+    const response = await fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8e13b136facec361d2037e0504bce3a1&per_page=10&format=json&nojsoncallback=1`,
+        {
+
+            //     headers:{
+
+            //     'authorization':'Client-ID 8e13b136facec361d2037e0504bce3a1'
+            // }
+
+        });
+
+
+
+
+    };
+
+
+
 
 
 getData();
