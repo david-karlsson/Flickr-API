020f5b0825e67976bdf51d05a83c71d2ec7e9617
David Karlsson <david.e.karlsson@telia.com>
2021-10-14 13:37:00 +0200

    page & per-page iteration-numbers added

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 const lista = document.querySelector('#lista');
 const form = document.querySelector('form');
-const searchTerm = document.querySelector('#search');//
-
-// form.addEventListener('submit', e => {
-
-//     e.preventDefault();
-
-//     search-list.innerHTML = '';
-//     getData(searchTerm.value);
+const searchTerm = document.querySelector('#search');
+const searchList = document.querySelector("#search-list");
+const Recent = document.querySelector('.Recent');
 
 
-// });
+form.addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    searchList.innerHTML = '';
+    getSearchData(searchTerm.value);
+    // showPhotos (getSearchData(searchTerm.value));
+
+
+});
+
+
+
+Recent.addEventListener('click', e => {
+
+    e.preventDefault();
+
+    lista.innerHTML = '';
+    getData();
+
+
+});
+
+
+
+  
 
 
 
 
-async function getData(query) {
 
-    const response = await fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=8e13b136facec361d2037e0504bce3a1&per_page=10&format=json&nojsoncallback=1`,
+
+async function getData() {
+
+    const response = await fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=8e13b136facec361d2037e0504bce3a1&per_page=5&format=json&nojsoncallback=1`,
         {
 
             //     headers:{
@@ -27,32 +49,46 @@ async function getData(query) {
 
         });
 
+        const data = await response.json();
+
+        const tagsResponse = await fetch(`https://api.flickr.com/services/rest/?method=flickr.tags.getListPhoto&api_key=8e13b136facec361d2037e0504bce3a1&per_page=5&format=json&nojsoncallback=1`,
+        {
+                 'photo_id':data.photos.photo[0].id
+           
+        });
 
 
 
 
 
+       
 
-    const data = await response.json();
+
     // showPhotos(data.results);
 
 
+    const tagStats = await tagsResponse.json();
+
 
  
+    const page = document.createElement('p');
+    form.appendChild(page);
+    
+    page.innerHTML = "Page: " + data.photos.page + "/" + data.photos.pages
+    +"<p>Photos per page: " + data.photos.perpage +"</p>";
+        
 
 
-
-
-     for (let index = 1; index < data.photos.pages.length; index++){
+    //  for (let index = 1; index < data.photos.pages.length; index++){
 
 
       
-        const page = document.createElement('h2');
-        form.appendChild(page);
+    //     const page = document.createElement('h2');
+    //     form.appendChild(page);
 
-        page.innerHTML = data.photos.page[index];
+    //     page.innerHTML = data.photos.page[index];
 
-     }; 
+    //  }; 
 
 
 
@@ -72,7 +108,7 @@ async function getData(query) {
 
         lista.appendChild(item);
         item.innerHTML = `
-        <h2> Image Title:"${data.photos.photo[index].title}"</h2>
+       <p>Image Title:</p> <h2> "${data.photos.photo[index].title}"</h2>
         <img src= ${SinglePhotoUrl} alt="${data.photos.photo[index].title}"/> `;
 
 
@@ -80,6 +116,8 @@ async function getData(query) {
 
     };
     console.log(data);
+    console.log(tagStats);
+    // return(data,tagStats);
 
 };
 
@@ -91,12 +129,37 @@ async function getSearchData(query) {
     const response = await fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8e13b136facec361d2037e0504bce3a1&per_page=10&format=json&nojsoncallback=1`,
         {
 
-            //     headers:{
+          
 
-            //     'authorization':'Client-ID 8e13b136facec361d2037e0504bce3a1'
-            // }
+            
 
         });
+
+        const data = await response.json();
+        showPhotos(data);
+
+        // for (let index = 0; index < data.photos.photo.length; index++) {
+
+        
+        //     // let SingePhotoObjectAdress = data.photos.photo[index]
+            
+        //     // let SinglePhotoUrl = "https://live.staticflickr.com/" + SingePhotoObjectAdress.server + "/" + SingePhotoObjectAdress.id +"_" +SingePhotoObjectAdress.secret+".jpg";
+    
+            
+    
+    
+        //     // const item = document.createElement('li');
+    
+        //     // lista.appendChild(item);
+        //     // item.innerHTML = `
+        //     // <h2> Image Title:"${data.photos.photo[index].title}"</h2>
+        //     // <img src= ${SinglePhotoUrl} alt="${data.photos.photo[index].title}"/> `;
+    
+    
+        //     // const element = array[index];
+    
+        // };
+
 
 
 
@@ -107,22 +170,38 @@ async function getSearchData(query) {
 
 
 
-getData();
 
-// const showPhotos = array => {
+const showPhotos = data => {
+
+    console.log(data);
+    console.log(tagStats);
+data.photos.photo.forEach(data => {
 
 
-// array.forEach(photo => {
-//     const item = document.createElement('li');
-//     item.innerHTML = `<p>${photo.alt_description}</p><img src="${photo.urls.thumb}" alt="${photo.alt_description}">`;
-//      lista.appendChild(item);
-// });
+    let SingePhotoObjectAdress = data.photos.photo[index]
+            
+    let SinglePhotoUrl = "https://live.staticflickr.com/" + SingePhotoObjectAdress.server + "/" + SingePhotoObjectAdress.id +"_" +SingePhotoObjectAdress.secret+".jpg";
 
-// };
+    
+
+
+    const item = document.createElement('li');
+
+    lista.appendChild(item);
+    item.innerHTML = `
+    <h2> Image Title:"${data.photos.photo[index].title}"</h2>
+    <img src= ${SinglePhotoUrl} alt="${data.photos.photo[index].title}"/> `;
+
+
+
+
+  
+});
+
+};
 
 
 
 
 
 // console.log(array)
-// getData();
